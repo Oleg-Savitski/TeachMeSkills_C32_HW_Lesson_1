8a8a2618eba2490d5b20604cd681c6b7025842c0
Oleg-Savitski <zoogostinica2016@gmail.com>
2024-10-14 16:22:41 +0300

    Update Task_2_Lesson_1.java

--- src/com/techmeskills/lesson_1/Task_2_Lesson_1.java
+++ src/com/techmeskills/lesson_1/Task_2_Lesson_1.java
@@ -1,4 +1,16 @@
 package com.techmeskills.lesson_1;
 
+/** В переменной n хранится двузначное число. Создайте программу, вычисляющую и
+ * выводящую на экран сумму цифр n. Например: n =26, в результате мы должны получить 8
+ * (2+6).
+ */
+
 public class Task_2_Lesson_1 {
+    public static void main(String[] args) {
+        int n = 26;
+        int a = n / 10;
+        int b = n - a * 10;
+        int sum = a + b;
+        System.out.println("Answer: the sum of the numbers = " + sum);
+    }
 }
