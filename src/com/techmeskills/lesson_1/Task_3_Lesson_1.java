9fffc3df1bfb6a1b6a47b96db185c41398a05317
Oleg-Savitski <zoogostinica2016@gmail.com>
2024-10-14 16:22:06 +0300

    Update Task_3_Lesson_1.java

--- src/com/techmeskills/lesson_1/Task_3_Lesson_1.java
+++ src/com/techmeskills/lesson_1/Task_3_Lesson_1.java
@@ -1,4 +1,18 @@
 package com.techmeskills.lesson_1;
 
+/** В переменной n хранится трёхзначное число. Создайте программу, вычисляющую и
+ * выводящую на экран сумму цифр n. Например: n =126, в результате мы должны получить
+ * 9 (1+2+6).
+ */
+
 public class Task_3_Lesson_1 {
+    public static void main(String[] args) {
+        int n, a, b, c, sum;
+        n = 111;
+        a = n / 100;
+        b = (n / 10) % 10;
+        c = n % 10;
+        sum = a + b + c;
+        System.out.println("The sum of the digits of the three-digit number 111 = " + sum);
+    }
 }
