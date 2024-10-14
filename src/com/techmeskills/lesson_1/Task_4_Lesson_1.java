500f9b468aac84da16b73733102e4d4050f792f9
Oleg-Savitski <zoogostinica2016@gmail.com>
2024-10-14 16:20:14 +0300

    Update Task_4_Lesson_1.java

--- src/com/techmeskills/lesson_1/Task_4_Lesson_1.java
+++ src/com/techmeskills/lesson_1/Task_4_Lesson_1.java
@@ -1,4 +1,14 @@
 package com.techmeskills.lesson_1;
 
+/** В переменной n хранится вещественное число с ненулевой дробной частью. Создайте
+ * программу, округляющую число n до ближайшего целого и выводящую результат на
+ * экран.
+ */
+
 public class Task_4_Lesson_1 {
+    public static void main(String[] args) {
+        double n = 99.56789;
+        long a = Math.round(n);
+        System.out.println("The nearest round number: " + a);
+    }
 }
