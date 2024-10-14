757f834d301c8435f450969f5958a911450b8797
Oleg-Savitski <zoogostinica2016@gmail.com>
2024-10-14 16:21:24 +0300

    Update Task_5_Lesson_1.java

--- src/com/techmeskills/lesson_1/Task_5_Lesson_1.java
+++ src/com/techmeskills/lesson_1/Task_5_Lesson_1.java
@@ -1,4 +1,17 @@
 package com.techmeskills.lesson_1;
 
-public class Task_5_Lesson_1 {
+/** В переменных q и w хранятся два натуральных числа. Создайте программу, выводящую
+     * на экран результат деления q на w с остатком. Пример вывода программы (для случая,
+     * когда в q хранится 21, а в w хранится 8): 21 / 8 = 2 и 5 в остатке.
+     */
+
+    public static class Task_5_Lesson_1 {
+        public static void main(String[] args) {
+            int a, b, c;
+            a = 11;
+            b = 3;
+            c = (a % b);
+            System.out.println("The remainder of the division of the number " + a + " / " + b + " = " + c);
+        }
+    }
 }
